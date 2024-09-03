ae66f654c8adcf87523cace4c8229f56c497552a
Mateo Isabey <mateoisabey@gmail.com>
2024-09-03 17:40:18 -0400

    column

--- HappyBirthday/app/src/main/java/com/tp/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/tp/happybirthday/MainActivity.kt
@@ -3,6 +3,8 @@ package com.tp.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,16 +35,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Mat!")
+        GreetingText(message = "Happy Birthday Mat!", from = "Emma")
     }
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column (modifier = modifier) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
 
-    )
+            )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+
+            )
+    }
+
 }
