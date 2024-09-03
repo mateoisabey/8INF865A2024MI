00da2f4db5688f317e21d08f2ec92da84b32f9ab
Mateo Isabey <mateoisabey@gmail.com>
2024-09-03 17:32:22 -0400

    Happy Birthday Mat

--- HappyBirthday/app/src/main/java/com/tp/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/tp/happybirthday/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,5 +32,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
+        GreetingText(message = "Happy Birthday Mat!")
     }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message
+    )
 }
