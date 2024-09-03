268f98f9af626e472e7539d341451a3da9231ef0
Mateo Isabey <mateoisabey@gmail.com>
2024-09-03 17:34:53 -0400

    size

--- HappyBirthday/app/src/main/java/com/tp/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/tp/happybirthday/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.tp.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +40,9 @@ fun BirthdayCardPreview() {
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
+
     )
 }
