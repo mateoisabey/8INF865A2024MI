94eb52ccb468c4a5996f9562044c04b0eae54652
Mateo Isabey <mateoisabey@gmail.com>
2024-09-24 15:14:50 -0400

    route entre les page

--- basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
+++ basic-android-kotlin-compose-training-cupcake-starter/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
@@ -49,10 +49,12 @@ import com.example.cupcake.ui.theme.CupcakeTheme
 @Composable
 fun StartOrderScreen(
     quantityOptions: List<Pair<Int, Int>>,
+    onNextButtonClicked: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier,
+
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Column(
@@ -83,7 +85,7 @@ fun StartOrderScreen(
             quantityOptions.forEach { item ->
                 SelectQuantityButton(
                     labelResourceId = item.first,
-                    onClick = {}
+                    onClick = { onNextButtonClicked(item.second) }
                 )
             }
         }
@@ -114,6 +116,7 @@ fun StartOrderPreview() {
     CupcakeTheme {
         StartOrderScreen(
             quantityOptions = DataSource.quantityOptions,
+            onNextButtonClicked = {},
             modifier = Modifier
                 .fillMaxSize()
                 .padding(dimensionResource(R.dimen.padding_medium))
